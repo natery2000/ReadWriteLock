c42aee68815a5ae1ffe9c1955a32a8ce6fcf829b
Nathan Werry <natery2000@hotmail.com>
2019-02-16 11:00:20 -0600

    Initial change

---
+++ Natery.ReadWriteLock/Natery.ReadWriteLock/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+
+namespace Natery.ReadWriteLock
+{
+  public class LockedResource<TResource>
+  {
+    private TResource _resource;
+    private ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+
+    public LockedResource(TResource resource)
+    {
+      _resource = resource;
+    }
+
+    public TResource Read()
+    {
+      _lock.EnterReadLock();
+      try
+      {
+        return _resource;
+      }
+      finally
+      {
+        _lock.ExitReadLock();
+      }
+    }
+
+    public void Write(TResource value)
+    {
+      _lock.EnterWriteLock();
+      try
+      {
+        _resource = value;
+      }
+      finally
+      {
+        _lock.ExitWriteLock();
+      }
+    }
+  }
+}
